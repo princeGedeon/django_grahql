67fe958be2369f0e49fadcc95d9d30236d8f4b65
princeGedeon <guedjegedeon03@gmail.com>
2023-03-10 01:35:45 -0800

    initiation basique

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from books.models import Books
+
+
+@admin.register(Books)
+class BookAdmin(admin.ModelAdmin):
+    pass
--- books/schema.py
+++ books/schema.py
@@ -6,3 +6,11 @@ class BooksType(DjangoObjectType):
     class Meta:
         model=Books
         fields=("id","title","content")
+
+class Query(graphene.ObjectType):
+    all_books=graphene.List(BooksType)
+
+    def resolve_all_books(root,info):
+        return Books.objects.all()
+
+schema=graphene.Schema(query=Query)
